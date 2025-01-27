b9d22b3f534a3ff568f3f84a60cb72120d31fbf5
Amir Mostafa <amirabdelaal73@gmail.com>
2025-01-27 19:16:27 +0200

    main merge

--- cp.cpp
+++ cp.cpp
@@ -4,30 +4,49 @@
 typedef long long ll;
 using namespace std;
 
-int main() {
-    fast
-    string s;
-    cin>>s;
-    ll num=stoll(s);
-    ll mini=1e12;
-    for (int len=1;len<12;len++){
-        for (int msk=0;msk<(1<<len);msk++){
-            string curr="";
-            int _4=0,_7=0;
-            for (int i=0;i<len;i++){
-                if (msk & (1<<i)) {
-                    curr+='4';
-                    _4++;
-                }  
-                else{
-                    curr+='7';
-                    _7++;
-                }   
-            }
-            if (_4==_7 && stoll(curr)>= num)    mini=min(mini,stoll(curr));
-        }
-    }
-    cout<<mini;
-    return 0;
+int dx[8]={0,0,1,-1,1,-1,1,-1};
+int dy[8]={1,-1,0,0,1,1,-1,-1};
+int n;
+
+bool valid (int x,int y,vector<vector<int>>&grid,vector<vector<bool>>&vis){
+    return x>=0 && x<n && y>=0 && y<n && !vis[x][y] && grid[x][y]==1;
 }
 
+void dfs (int x,int y,vector<vector<int>>&grid,vector<vector<bool>>&vis){
+    vis[x][y]=1;
+    for (int i=0;i<8;i++){
+        int nx=x+dx[i];
+        int ny=y+dy[i];
+        if(valid(nx,ny,grid,vis)){
+            dfs (nx,ny,grid,vis);
+        }
+    }
+}
+
+int main() {
+    fast
+    int tc=1;
+    while (cin>>n){
+        vector<vector<int>>grid(n,vector<int>(n));
+        vector<vector<bool>>vis(n,vector<bool>(n,0));
+        int cnt=0;
+        for (int i=0;i<n;i++){
+            string s;
+            cin>>s;
+            for (int j=0;j<n;j++){
+                grid[i][j]=s[j]-'0';
+            }
+        }
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n;j++){
+                if (grid[i][j]==1 && !vis[i][j]){
+                    dfs (i,j,grid,vis);
+                    cnt++;
+                }
+            }
+        }
+        cout<<"Image number "<<tc<<" contains "<<cnt<<" war eagles.\n";
+        tc++;
+    }
+    return 0;
+}
